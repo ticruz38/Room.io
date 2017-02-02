a52b1849fd6062a83cf89a5953d00612c3120fd7
Ticruz <thib.duchene@gmail.com>
2017-02-02 15:56:30 +0100

    🐛🔫

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -6,56 +6,61 @@ import Signup from './Signup';
 import { Input, Field } from '../form';
 import { password, nonEmpty } from '../form/Constraint';
 import { layoutState } from '../layout/Layout';
-import Loader from '../graphql-client/Loader'; 
+import Loader from '../graphql-client/Loader';
 
 const Document = require('./Login.gql');
 
 
-
-
 class LoginState {
 
-  @observable _id: String;
+    @observable _id: String;
 
-  @observable name: Field = {
-    value: undefined,
-    constraints: [nonEmpty()],
-    isValid: false
-  };
+    @observable name: Field = {
+        value: undefined,
+        constraints: [nonEmpty()],
+        isValid: false
+    };
 
-  @observable password: Field = {
-    value: undefined,
-    constraints: [password()],
-    isValid: false
-  };
+    @observable password: Field = {
+        value: undefined,
+        constraints: [password()],
+        isValid: false
+    };
 
-  login() {
-      Loader.execute(Document, 'Login', )
-  }
+    login() {
+        Loader.execute(Document, 'Login', )
+    }
+
+    format() {
+        return {
+            "name": this.name.value,
+            "password": this.password.value
+        }
+    }
 }
 
 export const loginState = new LoginState();
 
-export default class Login extends React.Component< any, LoginState > {
-  render() {
-    return (
-      <div className='login'>
-        <Input
-          label='Name/Pseudo'
-          field={loginState.name}
-          type='text'
-        />
-        <Input
-          label='Password'
-          field={loginState.password}
-          type='text'
-        />
-        <div className="question">
-          <button onClick={ _ => layoutState.modal = <Signup/> }>Not a member yet ?</button>
-        </div>
-      </div>
-    );
-  }
+export default class Login extends React.Component<any, LoginState> {
+    render() {
+        return (
+            <div className='login'>
+                <Input
+                    label='Name/Pseudo'
+                    field={loginState.name}
+                    type='text'
+                />
+                <Input
+                    label='Password'
+                    field={loginState.password}
+                    type='text'
+                />
+                <div className="question">
+                    <button onClick={_ => layoutState.modal = <Signup />}>Not a member yet ?</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 import './Login.scss';
