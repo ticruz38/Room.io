913c40f8ce27c8c8e667747be852ff6b169b0a28
Ticruz <thib.duchene@gmail.com>
2017-02-05 23:15:01 +0100

    Dropdown type fix

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -10,18 +10,25 @@ type DropdownProps = {
 
 export default class Dropdown extends React.Component< DropdownProps, {expand: boolean} > {
     state = { expand: false };
+
+    expand() {
+        const list: any = this.refs['list'];
+        this.setState({expand: !this.state.expand});
+        list.focus()
+    }
+    
     render() {
         return (
             <div className="dropdown"
                 style={{ justifyContent: this.props.align === 'right' ? 'flex-end' : 'flex-start'}}>
-                <span onClick={ _ => this.setState( { expand: !this.state.expand }, () => this.refs['list'].focus() ) }>{this.props.button}</span>
+                <span onClick={ _ => this.expand() }>{this.props.button}</span>
                 <div
                     ref='list'
                     tabIndex={-1}
                     className={ classnames("dropdown-list", { hidden: !this.state.expand}) }
                     onBlur={ _ => this.setState( { expand: false } ) }
                 >
-                    { this.props.list.map( l => <div style={{ justifyContent: this.props.align === 'right' ? 'flex-end' : 'flex-start'}}>{l}</div> ) }
+                    { this.props.list.map( (l, i) => <div key={i} style={{ justifyContent: this.props.align === 'right' ? 'flex-end' : 'flex-start'}}>{l}</div> ) }
                 </div>
             </div>
         );
