0e34f179efa3a01e1dfaa0f4e3d02f13780ae93e
Ticruz <thib.duchene@gmail.com>
2017-05-04 09:43:46 +0200

    app is ready when database are loaded

--- graph/IpfsApiStore.ts
+++ graph/IpfsApiStore.ts
@@ -3,98 +3,99 @@ import { EventEmitter } from 'events';
 // stream
 import * as through from 'through2';
 import * as concat from 'concat-stream';
+import * as Ipfs from 'ipfs';
+import Web3DB from './Web3DB';
 
-const IpfsApi = require('@haad/ipfs-api')
 
 // import IpfsDaemon from './IpfsDaemon';
-
-const Orbitdb = require('orbit-db');
 const Logger = require('logplease');
 
 const logger = Logger.create('Ipfs-server');
 
 class IpfsStore {
-    nodeID: string;
-    orbitdb: any;
+    web3DB: any; // layer on top of orbitdb
     ipfs: any;
-    room: Promise< any >;
-    stuff: Promise< any >;
-    user: Promise< any >;
-    order: Promise< any >;
-    chat: any;
     pubsub = new EventEmitter();
+    bootingDb: Promise<any> = this.startWeb3DB();
+    room: Promise<any> = this.createDb('room');
+    stuff: Promise<any> = this.createDb('stuff');
+    user: Promise<any> = this.createDb('user');
+    order: Promise<any> = this.createDb('order');
 
-    constructor() {
-        this.startOrbitDb();
-        this.createDb('room');
-        this.createDb('stuff');
-        this.createDb('user', 'email');
-        this.createDb('order');
+    get starting(): Promise<any> {
+        return Promise.all([
+            this.room,
+            this.stuff,
+            this.user,
+            this.order
+        ])
     }
 
-    // roomLoaded number between 0 and 1
-    roomLoading: number = 0;
-
-    // stuffLoaded number between 0 and 1
-    stuffLoading: number = 0;
-
-    uploadFile(input, cb?: (err, res: any ) => void ) {
+    uploadFile(input, cb?: (err, res: any) => void) {
         const reader = new FileReader();
-        reader.readAsArrayBuffer( input );
-        reader.onloadend = ( e ) => {
-            this.ipfs.files.add( new Buffer( reader.result ), (err, res) => {
-                if(err) throw 'file couldnt be upload to ipfs' + input
-                cb( err, res );
-            } );
+        reader.readAsArrayBuffer(input);
+        reader.onloadend = (e) => {
+            this.ipfs.files.add(new Buffer(reader.result), (err, res) => {
+                if (err) throw 'file couldnt be upload to ipfs' + input
+                cb(err, res);
+            });
         }
     }
 
-    getImage( hash: string ): Promise< any > {
-        return new Promise( (resolve, reject) => {
-            this.ipfs.files.get( hash, (err, stream) => {
-                if( err ) throw err;
+    getImage(hash: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.ipfs.files.get(hash, (err, stream) => {
+                if (err) throw err;
                 let files = [];
-                stream.pipe( through.obj( (file, enc, next) => {
-                    file.content.pipe( concat( (content) => {
-                        files.push( {
+                stream.pipe(through.obj((file, enc, next) => {
+                    file.content.pipe(concat((content) => {
+                        files.push({
                             path: file.path,
                             content: content
-                        } )
+                        })
                         next();
-                    } ) )
+                    }))
                 }, () => {
                     // the blob is invalid maybe because it is encoded as json...
-                    const file = new Blob( files[0].content, {type: 'image/jpg'} );
+                    const file = new Blob(files[0].content, { type: 'image/jpg' });
                     // console.log(file);
                     const reader = new FileReader();
                     reader.readAsDataURL(file)
                     // reader.onloadend = e => resolve(reader.result);
-                    resolve( btoa( String.fromCharCode.apply(null, files[0].content) ) );
-                } ) )
-            } );
-        } );
+                    resolve(btoa(String.fromCharCode.apply(null, files[0].content)));
+                }))
+            });
+        });
     }
 
-    createDb(dbName: string, indexBy?: string ) {
+    createDb(dbName: string, indexBy?: string): Promise<any> {
         console.log('createDb', dbName);
-        this[dbName] = new Promise( (resolve, reject) => {
-            const db = this.orbitdb.docstore(dbName, {indexBy: indexBy || '_id'});
-            window[dbName] = db;
-            db.events.on('ready', _ => {
-              resolve(db);
-              logger.info('db ' + dbName + ' ready')
-            } );
-            db.events.on('load.start', _ => logger.info('db ' + dbName + ' starting'));
-        } );
+        return new Promise((resolve, reject) => {
+            this.bootingDb.then(_ => {
+                const db = this.web3DB.docstore(dbName, { indexBy: indexBy || '_id' });
+                console.log(db);
+                db.load(); // to fetch current ipfs repo
+                window[dbName] = db;
+                db.events.on('ready', message => {
+                    resolve(db);
+                    logger.info('db ' + dbName + ' ready ' + message)
+                });
+                db.events.on('sync', _ => logger.info('db ' + dbName + ' starting'));
+            })
+            // return db.load().then( message => logger.info( 'db ' + dbName + ' ready ' + message ));
+        });
     }
 
-    startOrbitDb() {
-        // IpfsApi is a bridge to the local ipfs client node
-        this.ipfs = new IpfsApi();
-        // nodeId is the ipfs node identifier
-        this.nodeID = this.ipfs.id().then( (config: any) => this.nodeID = config.id );
-         // We instantiate Orbit-db with our ipfs client node
-        this.orbitdb = new Orbitdb( this.ipfs, "default", { ipns: "QmRiVcrZ7Jibn5CddvwE4UCGvQkDALy3e1h8aEUxu9PbcG"} );
+    startWeb3DB(): Promise<any> {
+        this.ipfs = new Ipfs({ EXPERIMENTAL: { pubsub: true } });
+        return new Promise((resolve, reject) => {
+            this.ipfs.on('ready', () => {
+                // We instantiate Orbit-db with our ipfs client node
+                this.web3DB = new Web3DB(this.ipfs);
+                resolve();
+            })
+            this.ipfs.on('error', (err) => reject(err))
+        })
     }
 }
 
---
+++ graph/Web3DB.ts
@@ -0,0 +1,74 @@
+import * as OrbitDB from 'orbit-db';
+import * as Ipfs from 'ipfs';
+
+const roomDataRequest = 'roomio:data:request';
+const roomDataUpdate = 'roomio:data:update';
+
+
+//TODO Make it listening to sinced db events and notify the guardian about the new hash
+export default class Web3DB extends OrbitDB {
+    plugging: Promise<any>
+    constructor(ipfs: Ipfs) {
+        super(ipfs);
+        window['web3'] = ipfs;
+        this.requestDataToGuard();
+    }
+    // plug the node to the roomio network to subscribe to generic network events
+    requestDataToGuard() {
+        this._ipfs.id().then(peer => {
+            this._ipfs.swarm.connect('/ip4/127.0.0.1/tcp/4003/ws/ipfs/QmXNgnNG5hdshrggnJ2GvhdE6p6LY6imhqP3vE9uDHZ21S', (err) => {
+                if (err) return console.error(err);
+                console.log('you just connected to the guardnode');
+                this._ipfs.swarm.peers((err, peerInfos) => console.log(peerInfos));
+                // for some reason we need to set a setTimeout...
+                setTimeout(_ => this._ipfs.pubsub.publish(roomDataRequest, new Buffer(peer.id)), 100);
+            });
+            this._ipfs.pubsub.subscribe(peer.id, message => {
+                const cache = JSON.parse(message.data.toString());
+                console.info('guardnode responded with cache', cache);
+                Object.keys( cache ).map( key => {
+                    this.stores[key]._cache.set(key, cache[key]).then(_ => {
+                        // reload the database with the new cache
+                        console.log('reloading database?') // yes but block is unfindable since it's not stocked in any repo neither this neither guardnode
+                        this.stores[key].load();
+                    })
+                })
+            })
+        })
+    }
+
+    /* Private methods to be overriden to subscribe to the sync event as well*/
+    _createStore(Store, dbname, options) {
+        const opts = Object.assign({ replicate: true }, options)
+
+        const store = new Store(this._ipfs, this.user.id, dbname, opts)
+        store.events.on('write', this._onWrite.bind(this))
+        store.events.on('ready', this._onReady.bind(this))
+        store.events.on('synced', this._onSync.bind(this))
+
+        this.stores[dbname] = store
+
+        if (opts.replicate && this._pubsub)
+            this._pubsub.subscribe(dbname, this._onMessage.bind(this))
+
+        return store
+    }
+
+    /* Data events */
+    _onWrite(dbname, hash, entry, heads) {
+        // 'New entry written to database...', after adding a new db entry locally
+        console.log(".WROTE", dbname, hash)
+        if (!heads) throw new Error("'heads' not defined")
+        if (this._pubsub) setImmediate(() => {
+            this._pubsub.publish(dbname, heads)
+            this._ipfs.pubsub.publish(roomDataUpdate, new Buffer( JSON.stringify( {[dbname]: hash} ) ) );
+        } )
+    }
+
+    // Notify the guard about the new hash
+    _onSync(dbName) {
+        const dbHash = { [dbName]: this.stores[dbName]._cache[dbName] }
+        console.log('.ONSync', dbHash);
+        this._ipfs.pubsub.publish(roomDataUpdate, new Buffer(JSON.stringify(dbHash)));
+    }
+}
