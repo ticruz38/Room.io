4aefeda5e5b727de30ef178a8992711ddaa40d24
Ticruz <thib.duchene@gmail.com>
2017-05-10 17:28:17 +0200

    switched to watchDB

--- graph/resolvers/Subscription.ts
+++ graph/resolvers/Subscription.ts
@@ -5,14 +5,14 @@ const logger = Logger.create('subscription')
 
 export default {
     watchOrders(root, { id }, store) {
-        return db.order.then(dborder => {
+        return db.orderWatch.then(dborder => {
             logger.info('order:subscription');
             return dborder.query(o => o.roomId === id) || [];
         });
     },
     // context here is the component state
     watchRooms(root, args, context) {
-        db.room.then(dbroom => {
+        db.roomWatch.then(dbroom => {
             const query = () => dbroom.query(doc => !!doc);
             dbroom.events.on('write', (dbname, hash, entry) => {
                 logger.info('watchRoom:subscription:write', dbname, hash, entry);
