feeb908518d524c169a0f4dfe3c9ea3075d27b8f
Ticruz <thib.duchene@gmail.com>
2017-05-22 10:33:45 +0200

    Button import

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Dropdown } from './Dropdown';
 export { default as IpfsImage } from './IpfsImage';
+export { default as Button } from './Button';
 export { default as Select } from './Select';
