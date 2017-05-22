941ec6a9bf14f521243db5afc6aa240c9e0053d2
Ticruz <thib.duchene@gmail.com>
2017-05-23 00:20:37 +0200

    moving to live testnet

--- graph/DbContract.ts
+++ graph/DbContract.ts
@@ -1,35 +1,20 @@
 const Web3 = require('web3');
 const contract = require('truffle-contract');
-const database_artifact = require('/Users/tduchene/Code/truffle/build/contracts/DataBase.json');
-
-
+// const database_artifact = require('/Users/tduchene/Code/truffle/build/contracts/DataBase.json');
+const database = require('/Users/tduchene/Code/truffle/bin/contracts/DataBase.json');
+console.log(database);
 
 const DataBase: Promise<{instance: any, account: string}> = new Promise((resolve, reject) => {
-    const db = contract(database_artifact);
     // if we are in mist or metamask the var web3 is already there
     if (typeof web3 !== 'undefined') {
         web3 = new Web3(web3.currentProvider);
     } else {
         // set the provider you want from Web3.providers
-        window["web3"] = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        window["web3"] = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
     }
-
-    db.setProvider(web3.currentProvider);
-    web3.eth.getAccounts(function (err, accs) {
-        if (err != null) {
-            reject("There was an error fetching your accounts.");
-            return;
-        }
-
-        if (accs.length == 0) {
-            reject("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-            return;
-        }
-        const account = accs[0];
-        db.deployed().then(instance => {
-            resolve({instance, account});
-        })
-    });
+    const db = window['web3'].eth.contract(JSON.parse(database.abi));
+    const dbInstance = db.at(['0x330cd6C307E8B0aaCbbe3183C5273ef948D6005A']);
+    resolve(dbInstance);
 });
 
 export default DataBase;
