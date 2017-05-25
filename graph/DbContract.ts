530b01bc3a7a730558459f4c145e6c15abd0588d
Ticruz <thib.duchene@gmail.com>
2017-05-25 19:07:02 +0200

    using with metamask as well

--- graph/DbContract.ts
+++ graph/DbContract.ts
@@ -1,23 +1,41 @@
+import { Connect } from 'uport-connect';
+const uport = new Connect('Roomio');
 const Web3 = require('web3');
 const contract = require('truffle-contract');
-// const database_artifact = require('/Users/tduchene/Code/truffle/build/contracts/DataBase.json');
-const database = require('/Users/tduchene/Code/truffle/bin/contracts/DataBase.json');
 
-const DataBase: Promise<{instance: any, account: string}> = new Promise((resolve, reject) => {
+const database_artifact = require('/Users/tduchene/Code/truffle/build/contracts/DataBase.json');
+
+
+
+const DataBase = (): Promise<{ instance: any, credentials: any }> => new Promise((resolve, reject) => {
+    const db = contract(database_artifact);
     // if we are in mist or metamask the var web3 is already there
     if (typeof web3 !== 'undefined') {
         web3 = new Web3(web3.currentProvider);
+        db.setProvider(web3.currentProvider);
+        web3.eth.getAccounts((err, accs) => {
+            db.deployed().then(instance => {
+                const credentials = { address: accs[0] };
+                resolve({ instance, credentials });
+            })
+        })
     } else {
         // set the provider you want from Web3.providers
-        window["web3"] = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+        window["web3"] = uport.getWeb3();
+        db.setProvider(web3.currentProvider);
+        if (!sessionStorage.userId) {
+            return uport.requestCredentials().then(credentials => {
+                console.log(credentials);
+                db.deployed().then(instance => {
+                    resolve({ instance, credentials });
+                });
+            }).catch(error => reject(error));
+        }
+        db.deployed().then(instance => {
+            const credentials = { address: sessionStorage.userId };
+            resolve({ instance, credentials });
+        })
     }
-    const db = window['web3'].eth.contract(JSON.parse(database.abi));
-    // Contract is unfindable at the current address!!
-    const dbInstance = db.at(['0x330cd6C307E8B0aaCbbe3183C5273ef948D6005A']);
-    web3.eth.getAccounts( (err, accs ) => {
-        console.log(accs);
-        resolve( { instance: dbInstance, account: accs[0] });
-    } )
 });
 
 export default DataBase;
